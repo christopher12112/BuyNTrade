b895dda57c31c53aa7b7f86d46ebc39e9aedd63e
ag2329 <50148004+ag2329@users.noreply.github.com>
2020-02-23 11:30:51 -0500

    Update test.py

--- Front_end/test.py
+++ Front_end/test.py
@@ -1,1 +1,9 @@
-
+FROM ubuntu
+RUN apt-get update
+RUN apt-get install -y apache2
+RUN apt-get install -y apache2-utils
+RUN apt-get clean
+EXPOSE 80
+COPY index.html /var/www/html
+VOLUME /var/www/html
+CMD ["/usr/sbin/apache2ctl", "-DFOREGROUND"]
