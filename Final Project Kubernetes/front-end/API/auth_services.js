3e981e6e349363c412cd456fd57a73dfae72a9f6
ag2329 <50148004+ag2329@users.noreply.github.com>
2020-05-10 14:47:11 -0400

    Add files via upload

---
+++ Final Project Kubernetes/front-end/API/auth_services.js
@@ -0,0 +1,75 @@
+function do_login(type) {
+  let username = "";
+  let password = "";
+  let errorDiv = "";
+  let error_style = "";
+  if (type === "form") {
+    username = document.getElementById("login_username").value;
+    password = document.getElementById("login_password").value;
+    errorDiv = "error";
+  } else {
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+    errorDiv = "error1";
+  }
+  if (username != "" && password != "") {
+    $.get(
+      `http://localhost:8080/api/v1/namespaces/default/services/back-end:5001/proxy/login?username=${username}&password=${password}`,
+      function(data) {
+        let res = JSON.parse(data);
+
+        if (res.status === "error") {
+          document.getElementById(errorDiv).style.display = "block";
+          document.getElementById(errorDiv).innerHTML = res.message;
+          document.getElementById(errorDiv).style.color = "red";
+        } else {
+          document.getElementById(errorDiv).style.display = "block";
+          document.getElementById(errorDiv).innerHTML = "Login succesful";
+          document.getElementById(errorDiv).style.color = "green";
+          //We can also store the token for future use
+          localStorage.setItem("token", res.message);
+        }
+      }
+    );
+  } else {
+    document.getElementById(errorDiv).style.display = "block";
+    document.getElementById(errorDiv).innerHTML =
+      "Username and password are required!";
+    document.getElementById(errorDiv).style.color = "red";
+  }
+}
+
+function do_registration() {
+  let username = document.getElementById("rUsername").value;
+  let password = document.getElementById("rPassword").value;
+  let errorDiv = "rError";
+  console.log(document.getElementById(errorDiv))
+  if (username != "" && password != "") {
+    $.post(
+      `http://localhost:8080/api/v1/namespaces/default/services/back-end:5001/proxy/registration?username=${username}&password=${password}&email=&address=&contactNo=0`,
+      function(data) {
+		  console.log(res)
+		  console.log(data)
+        let res = JSON.parse(data);
+		
+		
+        if (res.status === "error") {
+          document.getElementById(errorDiv).style.display = "block";
+          document.getElementById(errorDiv).innerHTML = res.message;
+          document.getElementById(errorDiv).style.color = "red";
+        } else {
+          document.getElementById(errorDiv).style.display = "block";
+          document.getElementById(errorDiv).innerHTML = res.message;
+          document.getElementById(errorDiv).style.color = "green";
+          //We can also store the token for future use
+          localStorage.setItem("token", res.message);
+        }
+      }
+    );
+  } else {
+    document.getElementById(errorDiv).style.display = "block";
+    document.getElementById(errorDiv).innerHTML =
+      "Username and password are required!";
+    document.getElementById(errorDiv).style.color = "red";
+  }
+}
